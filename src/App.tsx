0119a8b31f0ae85cf2d5357822d62c20dd1b7fb0
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 14:34:49 +0200

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [showCart, setShowCart] = useState(false)
   return (
     <>
-      <Cart />
+      {showCart && <Cart onClose={() => setShowCart(false)} />}
       <Header />
       <main>
-        <Meals/>
+        <Meals />
       </main>
     </>
   );
