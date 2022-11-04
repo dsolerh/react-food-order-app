2cffd83e471921b84b869cf3c36b698dfa6b9438
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 14:09:40 +0200

    add Modal component

---
+++ src/components/UI/Modal.tsx
@@ -0,0 +1,67 @@
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import ContainerComponent from "../../types/ContainerComponent";
+import StyledComponent from "../../types/StyledComponent";
+
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 20;
+  background-color: rgba(0, 0, 0, 0.75);
+`
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 20vh;
+  left: 5%;
+  width: 90%;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 14px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+  z-index: 30;
+  animation: slide-down 300ms ease-out forwards;
+
+  @media (min-width: 768px) {
+    width: 40rem;
+    left: calc(50% - 20rem); 
+  }
+
+  @keyframes slide-down {
+    from {
+      opacity: 0;
+      transform: translateY(-3rem);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+`
+
+const ModalContent = styled.div`
+`
+
+function ModalOvarlay({ children, className }: ContainerComponent & StyledComponent) {
+  return (
+    <ModalWrapper className={className}>
+      <ModalContent>{children}</ModalContent>
+    </ModalWrapper>
+  );
+}
+
+function Modal({ children }: ContainerComponent) {
+  const portal = document.getElementById('overlay')!
+
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portal)}
+      {ReactDOM.createPortal(<ModalOvarlay>{children}</ModalOvarlay>, portal)}
+    </>
+  );
+}
+
+export default Modal;
