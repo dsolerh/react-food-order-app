54d2ffb6a4af378c55f9d3f70c5e043d9a34b18c
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 10:56:19 +0200

    add styles

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -29,3 +29,41 @@ export const ImageWrapper = styled.div`
         transform: rotateZ(-5deg) translateY(-4rem) translateX(-1rem);
     }
 `
+
+export const Button = styled.button`
+    cursor: pointer;
+    font: inherit;
+    border: none;
+    background-color: #4d1601;
+    color: white;
+    padding: 0.75rem 3rem;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 25px;
+    font-weight: bold;
+
+    &:hover,
+    &:active {
+        background-color: #2c0d00;
+    }
+
+    &:hover .badge,
+    &:active .badge {
+        background-color: #92320c;
+    }
+`
+
+export const ButtonIcon = styled.span`
+    width: 1.35rem;
+    height: 1.35rem;
+    margin-right: 0.5rem;
+`
+
+export const ButtonBadge = styled.span`
+    background-color: #b94517;
+    padding: 0.25rem 1rem;
+    border-radius: 25px;
+    margin-left: 1rem;
+    font-weight: bold;
+`
