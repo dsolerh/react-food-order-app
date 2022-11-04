03ba44552fd16d45fe26f520eb251dacc1c52e50
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 15:14:02 +0200

    update app to use cart-context

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CardProvider";
 
 function App() {
   const [showCart, setShowCart] = useState(false)
   return (
-    <>
+    <CartProvider>
       {showCart && <Cart onClose={() => setShowCart(false)} />}
       <Header onOpenCart={()=>setShowCart(true)}/>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
