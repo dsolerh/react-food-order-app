4c958318d60d23a5735fd20fb3593a55c0fe804d
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 16:22:14 +0200

    use CartContext

--- src/components/Layout/CartButton.tsx
+++ src/components/Layout/CartButton.tsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
+import { CartContext } from "../../store/cart-contex";
 import ClickableComponent from "../../types/ClickableComponent";
 import CartIcon from "../Cart/CartIcon";
 import { Button, ButtonBadge, ButtonIcon } from "./styles";
 
 function CartButton({ onClick }: ClickableComponent) {
+    const ctx = useContext(CartContext);
+    const total = ctx.items.reduce((prev, curr) => prev + curr.amount!, 0)
+
     return (
         <Button onClick={onClick}>
             <ButtonIcon><CartIcon /></ButtonIcon>
             <span>Your Cart</span>
-            <ButtonBadge className="badge">3</ButtonBadge>
+            <ButtonBadge className="badge">{total}</ButtonBadge>
         </Button>
     );
 }
