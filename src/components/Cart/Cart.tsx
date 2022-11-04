55d3a04a68fc2dac9fd5d5e44f041b4301e42679
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 14:34:44 +0200

    add onClose prop to Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,18 +1,22 @@
 import Modal from "../UI/Modal";
 import { CartActions, CartButton, CartButtonAlt, CartList, CartTotal } from "./styles";
 
-function Cart() {
+interface CartProps {
+    onClose?: () => void
+}
+
+function Cart({ onClose }: CartProps) {
     const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }]
-        .map(item=> (<li>{item.name}</li>))
+        .map(item => (<li>{item.name}</li>))
     return (
-        <Modal>
+        <Modal onClose={onClose}>
             <CartList>{cartItems}</CartList>
             <CartTotal>
                 <span>Total amount</span>
                 <span>35.63</span>
             </CartTotal>
             <CartActions>
-                <CartButtonAlt>Close</CartButtonAlt>
+                <CartButtonAlt onClick={() => onClose && onClose()}>Close</CartButtonAlt>
                 <CartButton>Order</CartButton>
             </CartActions>
         </Modal>
