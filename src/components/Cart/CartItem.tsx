3ca010f8e02461f597ee4bcbffc117d047ddbfe7
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 16:24:07 +0200

    add CartItem

---
+++ src/components/Cart/CartItem.tsx
@@ -0,0 +1,27 @@
+import FoodItem from "../../types/FoodItem";
+import { ItemActions, ItemSummary, ListItem } from "./styles";
+
+interface CartItemProps extends Partial<FoodItem> {
+    onRemove: ()=>void
+    onAdd: ()=>void
+}
+
+function CartItem({ name, price, amount, onAdd, onRemove }: CartItemProps) {
+    return (
+        <ListItem>
+            <div>
+                <h2>{name}</h2>
+                <ItemSummary>
+                    <span className='price'>{price}</span>
+                    <span className='amount'>x {amount}</span>
+                </ItemSummary>
+            </div>
+            <ItemActions>
+                <button onClick={onRemove}>−</button>
+                <button onClick={onAdd}>+</button>
+            </ItemActions>
+        </ListItem>
+    );
+}
+
+export default CartItem;
