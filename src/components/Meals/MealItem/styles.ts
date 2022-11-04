e67e193edf79abe566c4737435358e7ee2de2024
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 11:32:19 +0200

    add some styles to MealItem component

---
+++ src/components/Meals/MealItem/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Meal = styled.li`
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #ccc;
+
+    & h3 {
+        margin: 0 0 0.25rem 0;
+    }
+`
+
+export const MealDescription = styled.div`
+    font-style: italic;
+`
+
+export const MealPrice = styled.div`
+    margin-top: 0.25rem;
+    font-weight: bold;
+    color: #ad5502;
+    font-size: 1.25rem;
+`
