eae353f463b53926f9a3b36fa914ff3fcd375365
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-04 14:26:47 +0200

    add style for the total

--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -8,19 +8,27 @@ export const CartList = styled.ul`
     overflow: auto;
 `
 
+export const CartTotal = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin: 1rem 0;
+`
+
 export const CartActions = styled.div`
     text-align: right;
-
+    
     & button {
         font: inherit;
         cursor: pointer;
-        background-color: transparent;
         border: 1px solid #8a2b06;
         padding: 0.5rem 2rem;
         border-radius: 25px;
         margin-left: 1rem;
     }
-
+    
     & button:hover,
     & button:active {
         background-color: #5a1a01;
@@ -35,5 +43,6 @@ export const CartButton = styled.button`
 `
 
 export const CartButtonAlt = styled.button`
+    background-color: transparent;
     color: #8a2b06;
 `
