391d1d9d6aafba276fe9b637fc9d6e8688ca74c9
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-07 10:39:15 +0200

    add animation to the cart button

--- src/components/Layout/CartButton.tsx
+++ src/components/Layout/CartButton.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../../store/cart-contex";
 import ClickableComponent from "../../types/ClickableComponent";
 import CartIcon from "../Cart/CartIcon";
@@ -6,10 +6,26 @@ import { Button, ButtonBadge, ButtonIcon } from "./styles";
 
 function CartButton({ onClick }: ClickableComponent) {
     const ctx = useContext(CartContext);
+    const [bumpButton, setBumpButton] = useState(false)
     const total = ctx.items.reduce((prev, curr) => prev + curr.amount!, 0)
 
+
+    useEffect(() => {
+        if (ctx.items.length !== 0) {
+            setBumpButton(true);
+        }
+
+        const t = setTimeout(()=>{
+            setBumpButton(false);
+        },300)
+
+        return () => {
+            clearTimeout(t)
+        }
+    }, [ctx.items])
+
     return (
-        <Button onClick={onClick}>
+        <Button onClick={onClick} className={bumpButton ? 'bump' : ''}>
             <ButtonIcon><CartIcon /></ButtonIcon>
             <span>Your Cart</span>
             <ButtonBadge className="badge">{total}</ButtonBadge>
