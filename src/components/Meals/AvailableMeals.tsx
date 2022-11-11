55faf6fa47755cb38f1d73376222b5dbe6acc1a2
dsolerh <dsolerh.cinter95@gmail.com>
2022-11-11 14:35:33 +0200

    fetch the data from firebase

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,40 +1,68 @@
+import { useEffect, useState } from "react";
+import FoodItem from "../../types/FoodItem";
 import { Card } from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import { AvailableMealsSection } from "./styles";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+// const DUMMY_MEALS = [
+//     {
+//         id: 'm1',
+//         name: 'Sushi',
+//         description: 'Finest fish and veggies',
+//         price: 22.99,
+//     },
+//     {
+//         id: 'm2',
+//         name: 'Schnitzel',
+//         description: 'A german specialty!',
+//         price: 16.5,
+//     },
+//     {
+//         id: 'm3',
+//         name: 'Barbecue Burger',
+//         description: 'American, raw, meaty',
+//         price: 12.99,
+//     },
+//     {
+//         id: 'm4',
+//         name: 'Green Bowl',
+//         description: 'Healthy...and green...',
+//         price: 18.99,
+//     },
+// ];
+
+type Meals = Required<Omit<FoodItem, 'amount'>>[]
 
 function AvailableMeals() {
-    const meals = DUMMY_MEALS.map(meal=> (
-        <MealItem 
+    const [meals, setMeals] = useState<Meals>([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('https://react-complete-course-c5824-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            const data = await response.json()
+
+            const meals: Meals = [];
+            for (const key in data) {
+                meals.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price,
+                })
+            }
+
+            return meals
+        })().then((data) => {
+            setMeals(data);
+        })
+
+    }, [])
+
+    const mealsList = meals.map(meal => (
+        <MealItem
             id={meal.id}
-            key={meal.id} 
-            name={meal.name} 
+            key={meal.id}
+            name={meal.name}
             description={meal.description}
             price={meal.price}
         />
@@ -43,7 +71,7 @@ function AvailableMeals() {
     return (
         <AvailableMealsSection>
             <Card>
-                <ul>{meals}</ul>
+                <ul>{mealsList}</ul>
             </Card>
         </AvailableMealsSection>
     );
